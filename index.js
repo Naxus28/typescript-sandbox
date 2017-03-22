ef759e531ea9f89623f8b48556b5d6df87cd4c82
Gabriel Ferraz <gferraz@fullsail.com>
2017-03-22 12:10:49 -0400

    removes arrays

--- index.js
+++ index.js
@@ -7,14 +7,3 @@ tuple = ['Hello', 10]; // initialize it
 tuple[3] = 5; // assign elements outside of the known indices assigned -- has to be either of the types expressed on the declaration otherwise it will error out
 console.log('tuple: ', tuple); // [ 'Hello', 10, , 5 ] -- notice it is missing index 2, which is assigned an empty string
 console.log('tuple length: ', tuple.length); // 4
-
-// need to declare and initialize array on the same statement
-var list = [1, 2, 3];
-var listOne = [];
-var listTwo; //this returns type 'undefined'; it is useless because since it is not an array we cannot assign any values to it
-console.log(list);
-console.log(listOne);
-console.log(listTwo); // undefined
-// listTwo[0] = 10; // Cannot set property '0' of undefined
-
-
--- index.ts
+++ index.ts
@@ -9,13 +9,3 @@ tuple[3] = 5; // assign elements outside of the known indices assigned -- has to
 console.log('tuple: ', tuple); // [ 'Hello', 10, , 5 ] -- notice it is missing index 2, which is assigned an empty string
 console.log('tuple length: ', tuple.length); // 4
 
-// need to declare and initialize array on the same statement
-let list: number[] = [1, 2, 3];
-let listOne: number[] = [];
-let listTwo: number[]; //this returns type 'undefined'; it is useless because since it is not an array we cannot assign any values to it
-
-console.log(list);
-console.log(listOne);
-console.log(listTwo); // undefined
-// listTwo[0] = 10; // Cannot set property '0' of undefined
-
