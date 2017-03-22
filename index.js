b6b5f0fad860e068b710d84ad8a345ee620d26f0
Gabriel Ferraz <gferraz@fullsail.com>
2017-03-22 14:03:31 -0400

    adds code

--- index.js
+++ index.js
@@ -1,20 +1,33 @@
-
-
-//Tuple types allow us to express an array where the type of a fixed number of elements is known, but need not be the same. 
-//For example, we can have a string and a number
-var tuple; // declare the tuple
-tuple = ['Hello', 10]; // initialize it
-tuple[3] = 5; // assign elements outside of the known indices assigned -- has to be either of the types expressed on the declaration otherwise it will error out
-console.log('tuple: ', tuple); // [ 'Hello', 10, , 5 ] -- notice it is missing index 2, which is assigned an empty string
-console.log('tuple length: ', tuple.length); // 4
-
-// need to declare and initialize array on the same statement
-var list = [1, 2, 3];
-var listOne = [];
-var listTwo; //this returns type 'undefined'; it is useless because since it is not an array we cannot assign any values to it
-console.log(list);
-console.log(listOne);
-console.log(listTwo); // undefined
-// listTwo[0] = 10; // Cannot set property '0' of undefined
-
-
+//In TypeScript, both undefined and null actually have their own types named undefined and null respectively. 
+//Much like void, they’re not extremely useful on their own:
+var u = undefined;
+var n = null;
+console.log(u); // undefined
+console.log(n); // null
+// By default null and undefined are subtypes of all other types. 
+// That means you can assign null and undefined to something like number or string.
+var num = undefined;
+var str = null;
+console.log(num); // undefined
+console.log(str); // null
+//reassign types
+num = 10;
+str = 'now i have a string value';
+console.log(num); // 10
+console.log(str); // 'now i have a string value'
+/*
+ * Important note:
+ * When using the --strictNullChecks flag (in tscConfig.json), 'null' and 'undefined' are only assignable to void and their respective types.
+ * This helps avoid many common errors.
+ * In cases where we want to pass in either a string or null or undefined, you can use the union type string | null | undefined.
+ * they encourage the use of --strictNullChecks when possible.
+ */
+// using union type
+function notSureWhatWillCome(someValue) {
+    return someValue;
+}
+// now we can call notSureWhatWillComewith either specified type
+console.log(notSureWhatWillCome('I am a string')); // I am a string
+console.log(notSureWhatWillCome(null)); // null
+console.log(notSureWhatWillCome(undefined)); // undefined
+console.log(notSureWhatWillCome(100)); // 100
--- index.ts
+++ index.ts
@@ -1,21 +1,41 @@
+//In TypeScript, both undefined and null actually have their own types named undefined and null respectively. 
+//Much like void, they’re not extremely useful on their own:
 
-//Tuple types allow us to express an array where the type of a fixed number of elements is known, but need not be the same. 
-//For example, we can have a string and a number
+let u: undefined = undefined;
+let n: null = null;
 
-let tuple: [string, number]; // declare the tuple
-tuple = ['Hello', 10]; // initialize it
-tuple[3] = 5; // assign elements outside of the known indices assigned -- has to be either of the types expressed on the declaration otherwise it will error out
+console.log(u); // undefined
+console.log(n); // null
 
-console.log('tuple: ', tuple); // [ 'Hello', 10, , 5 ] -- notice it is missing index 2, which is assigned an empty string
-console.log('tuple length: ', tuple.length); // 4
+// By default null and undefined are subtypes of all other types. 
+// That means you can assign null and undefined to something like number or string.
+let num: number = undefined;
+let str: string = null;
 
-// need to declare and initialize array on the same statement
-let list: number[] = [1, 2, 3];
-let listOne: number[] = [];
-let listTwo: number[]; //this returns type 'undefined'; it is useless because since it is not an array we cannot assign any values to it
+console.log(num); // undefined
+console.log(str); // null
 
-console.log(list);
-console.log(listOne);
-console.log(listTwo); // undefined
-// listTwo[0] = 10; // Cannot set property '0' of undefined
+//reassign types
+num = 10;
+str = 'now i have a string value';
 
+console.log(num); // 10
+console.log(str); // 'now i have a string value'
+
+/*
+ * Important note: 
+ * When using the --strictNullChecks flag (in tscConfig.json), 'null' and 'undefined' are only assignable to void and their respective types. 
+ * This helps avoid many common errors. 
+ * In cases where we want to pass in either a string or null or undefined, you can use the union type string | null | undefined. 
+ * they encourage the use of --strictNullChecks when possible.
+ */
+// using union type
+function notSureWhatWillCome(someValue: string | null | undefined | number) {
+  return someValue;
+}
+
+// now we can call notSureWhatWillComewith either specified type
+console.log(notSureWhatWillCome('I am a string')); // I am a string
+console.log(notSureWhatWillCome(null)); // null
+console.log(notSureWhatWillCome(undefined)); // undefined
+console.log(notSureWhatWillCome(100)); // 100
