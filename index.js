eae6b046dc330d0ddc11f769a99d0600e71cd002
Gabriel Ferraz <gferraz@fullsail.com>
2017-03-22 12:07:52 -0400

    adds code

---
+++ index.js
@@ -0,0 +1,10 @@
+// when we don't know the type beforehand (the value may be created dynamically) we assign the type 'any'
+// this prevents type checking during compilation so we can get away with not assigning types in certain cases
+var uncertain = 4;
+console.log(typeof uncertain); // number
+uncertain = false;
+console.log(typeof uncertain); // boolean
+uncertain = 'Now I am a string';
+console.log(typeof uncertain); // string
+uncertain = {};
+console.log(typeof uncertain); // object
