907b16d508388e9849b694cb05e1b75466453e81
Gabriel Ferraz <gferraz@fullsail.com>
2017-03-23 14:55:18 -0400

    adds code

--- index.js
+++ index.js
@@ -1,63 +1,24 @@
-//////////////////////
-function createSquare(config) {
-    var newSquare = { color: "white", area: 100 };
-    if (config.color) {
-        newSquare.color = config.color;
-    }
-    if (config.width) {
-        newSquare.area = config.width * config.width;
-    }
-    return newSquare;
-}
-var mySquare = createSquare({ color: "black", width: 100 });
 /**
- * Combining optional properties naively would let you to shoot yourself
- * in the foot the same way you might in JavaScript though.
+ * Readonly properties Some properties should only be modifiable when an object is first created.
+ * You can specify this by putting readonly before the name of the property:
  */
+// You can construct a Point by assigning an object literal. 
+// After the assignment, x and y can’t be changed.
+var p1 = { x: 10, y: 20 };
+p1.x = 5; // error! 
 /**
- * i.e:
- * You could argue that this program below is correctly typed, since the width properties are compatible,
- * there’s no color property present, and the extra colour property is insignificant.
- * However, TypeScript takes the stance that there’s probably a bug in this code.
- * Object literals get special treatment and undergo excess property checking when assigning them to other variables,
- * or passing them as arguments.
+ * TypeScript comes with a ReadonlyArray<T> type that is the same as Array<T>
+ * with all mutating methods removed, so you can make sure you don’t change your arrays after creation:
  */
-var mySquareTwo = createSquare({ colour: "red", width: 100 }); // error!
-// we can get around these checks in 3 different ways:
-// 1. using type assertion on the argument
-var squareOne = createSquare({ width: 100, opacity: 0.5 });
-// or we can  assign the object to another variable: 
-// Since squareOptions won’t undergo excess property checks, the compiler won’t give you an error.
-var squareOptions = { colour: "red", width: 100 };
-var squareTwo = createSquare(squareOptions);
-function createPerson(person) {
-    var newPerson = {};
-    for (var prop in person) {
-        newPerson[prop] = person[prop];
-    }
-    return newPerson;
-}
-var joe = createPerson({ name: 'Joe', age: 30, ethnicity: 'Latin' });
-console.log('joe: ', joe);
-var leo = createPerson({ name: 'Leo', age: 30, height: 6.2, weight: '180 lbs', married: true });
-console.log('leo: ', leo);
-/**
-without interface the code would be something like this
-
-function createPerson(name: string, age: number, otherFeatures: any) {
-  let newPerson = { name, age };
-
-  for (var prop in otherFeatures) {
-    newPerson[prop] = otherFeatures[prop];
-  }
-
-  return newPerson;
-}
-
-let joe = createPerson('Joe', 30, { ethnicity: 'Latin'} );
-console.log('joe: ', joe);
-
-
-let leo = createPerson('Leo', 30, {height: 6.2, weight: '180 lbs', married: true});
-console.log('leo: ', leo);
- */
+var a = [1, 2, 3, 4];
+var ro = a;
+// ro[0] = 12; // error!
+// ro.push(5); // error!
+// ro.length = 100; // error!
+// a = ro; // error!
+//we can override the array assignment with an assertion though
+var b = ro;
+// or
+b = ro;
+console.log('b: ', b); //[1, 2, 3, 4]
+///////////////////////  
--- index.ts
+++ index.ts
@@ -1,111 +1,36 @@
-//////////////////////
-
-/*
-Optional Properties
-Not all properties of an interface may be required. 
-Some exist under certain conditions or may not be there at all. 
-These optional properties are popular when creating patterns like “option bags” where you pass an object to a function that only has a couple of properties filled in.
-Here’s an example of this pattern:
+/**
+ * Readonly properties Some properties should only be modifiable when an object is first created. 
+ * You can specify this by putting readonly before the name of the property:
  */
 
-interface SquareConfig {
-    color?: string;
-    width?: number;
+interface Point {
+  readonly x: number;
+  readonly y: number;
 }
 
-function createSquare(config: SquareConfig): {color: string; area: number} {
-    let newSquare = {color: "white", area: 100};
-    if (config.color) {
-        newSquare.color = config.color;
-    }
-    if (config.width) {
-        newSquare.area = config.width * config.width;
-    }
-
-    return newSquare;
-}
-
-let mySquare = createSquare({color: "black", width: 100});
+// You can construct a Point by assigning an object literal. 
+// After the assignment, x and y can’t be changed.
+let p1: Point = { x: 10, y: 20 };
+p1.x = 5; // error! 
 
 /**
- * Combining optional properties naively would let you to shoot yourself 
- * in the foot the same way you might in JavaScript though. 
+ * TypeScript comes with a ReadonlyArray<T> type that is the same as Array<T> 
+ * with all mutating methods removed, so you can make sure you don’t change your arrays after creation:
  */
 
-/**
- * i.e:
- * You could argue that this program below is correctly typed, since the width properties are compatible, 
- * there’s no color property present, and the extra colour property is insignificant.
- * However, TypeScript takes the stance that there’s probably a bug in this code. 
- * Object literals get special treatment and undergo excess property checking when assigning them to other variables, 
- * or passing them as arguments.
- */
-let mySquareTwo = createSquare({ colour: "red", width: 100 }); // error!
+let a: number[] = [1, 2, 3, 4];
+let ro: ReadonlyArray<number> = a;
+// ro[0] = 12; // error!
+// ro.push(5); // error!
+// ro.length = 100; // error!
+// a = ro; // error!
 
-// we can get around these checks in 3 different ways:
-// 1. using type assertion on the argument
-let squareOne = createSquare({ width: 100, opacity: 0.5 } as SquareConfig);
+//we can override the array assignment with an assertion though
+let b: number[] = ro as number[];
+// or
+b = <number[]>ro;
 
-// 2. a better approach might be to add a string index signature if you’re sure 
-// that the object can have some extra properties that are used in some special way. 
-// If SquareConfigs can have color and width properties with the above types, 
-// but could also have any number of other properties, then we could define it like so:
-
-interface SquareConfig {
-  color?: string;
-  width?: number;
-  [propName: string]: any;
-}
-
-// or we can  assign the object to another variable: 
-// Since squareOptions won’t undergo excess property checks, the compiler won’t give you an error.
-let squareOptions = { colour: "red", width: 100 };
-let squareTwo = createSquare(squareOptions);
+console.log('b: ', b); //[1, 2, 3, 4]
 
 
-// my example -- a person has to have at least name and age and they need to match these types 
-// a person could also have any number of optional properties of any type
-interface Person {
-  name: string;
-  age: number;
-  [propName: string]: any;
-}
-
-function createPerson(person: Person): {name: string; age: number} {
-  let newPerson: any = {};
-
-  for (var prop in person) {
-    newPerson[prop] = person[prop];
-  }
-
-  return newPerson;
-}
-
-let joe = createPerson({name: 'Joe', age: 30, ethnicity: 'Latin'});
-console.log('joe: ', joe);
-
-
-let leo = createPerson({name: 'Leo', age: 30, height: 6.2, weight: '180 lbs', married: true});
-console.log('leo: ', leo);
-
-
-/**
-without interface the code would be something like this
-
-function createPerson(name: string, age: number, otherFeatures: any) {
-  let newPerson = { name, age };
-
-  for (var prop in otherFeatures) {
-    newPerson[prop] = otherFeatures[prop];
-  }
-
-  return newPerson;
-}
-
-let joe = createPerson('Joe', 30, { ethnicity: 'Latin'} );
-console.log('joe: ', joe);
-
-
-let leo = createPerson('Leo', 30, {height: 6.2, weight: '180 lbs', married: true});
-console.log('leo: ', leo);
- */
+/////////////////////// 
