42b95aea2aff813a5e99b6d63552a4126a346e7b
Gabriel Ferraz <gferraz@fullsail.com>
2017-03-24 13:27:20 -0400

    adds code

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
-/**
- * One of the most common uses of interfaces in languages like C# and Java,
- * that of explicitly enforcing that a class meets a particular contract, is also possible in TypeScript.
- */
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = Object.setPrototypeOf ||
+        ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+        function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 var Clock = (function () {
     function Clock(h, m) {
     }
@@ -19,10 +25,67 @@ var ClockOne = (function () {
 // type 'ClockTwo' provides no match for the signature 'new (hour: number, minute: number): any'"
 var ClockTwo = (function () {
     function ClockTwo(h, m) {
-    } // this is not an instance, but a static side of the class
+    } // this is not an instance, but the static side of the class
     return ClockTwo;
 }());
-/**
- * This is because when a class implements an interface, only the instance side of the class is checked.
- * Since the constructor sits in the static side, it is not included in this check.
- */
+// this is just a convenience method used to instantiate the classes
+// we can instantiate the classes directly if we want (i.e. new DigitalClock(7, 34, 'p.m.'))
+// it takes the 'ClockCtructor', the hour, and the minute, and returns an obj of type 'ClockIface'
+function createClock(ctor, hour, minute, timeOfTheDay) {
+    // needs to enforce that 'DigitalClock' and 'AnalogClock' meet the contract established on 'ClockCtructor'
+    return new ctor(hour, minute, timeOfTheDay);
+}
+// parent class from which we will extend other 'clock' classes
+var ClockParent = (function () {
+    function ClockParent(h, m, tOfTheDay) {
+        this.hour = h;
+        this.minute = m;
+        this.timeOfTheDay = tOfTheDay;
+    }
+    ClockParent.prototype.tick = function () { };
+    ClockParent.prototype.setTime = function () {
+        this.currentTime = this.hour + ':' + this.minute + ' ' + this.timeOfTheDay;
+    };
+    ClockParent.prototype.getTime = function () {
+        return this.currentTime;
+    };
+    return ClockParent;
+}());
+// instanciates parent class implicitly
+var DigitalClock = (function (_super) {
+    __extends(DigitalClock, _super);
+    function DigitalClock() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    // overrides parent method
+    DigitalClock.prototype.tick = function () {
+        return "beep beep: it is";
+    };
+    return DigitalClock;
+}(ClockParent));
+// instanciates parent class implicitly
+var AnalogClock = (function (_super) {
+    __extends(AnalogClock, _super);
+    function AnalogClock() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    // overrides parent method
+    AnalogClock.prototype.tick = function () {
+        return "tick tock: it is";
+    };
+    return AnalogClock;
+}(ClockParent));
+// creates instances of clock classes
+var digital = createClock(DigitalClock, 12, 17, 'p.m.'); // instantiates with convenience method
+var digitalTwo = new DigitalClock(7, 34, 'p.m.'); // this is the same as the above
+var analog = createClock(AnalogClock, 7, 32, 'a.m.');
+var clockLogger = function (clockInstance) {
+    clockInstance.setTime();
+    // clockInstance.constructor['name'] gets class name; type-checker yells if we use 'constructor.name'
+    // unless we assign type 'any' to the constructor and get the name from the variable:
+    // let class: any = digital.constructor; console.log(class.name);
+    console.log(clockInstance.constructor['name'] + ' ' + clockInstance.tick() + ' ' + clockInstance.getTime());
+};
+clockLogger(digital);
+clockLogger(digitalTwo);
+clockLogger(analog);
--- index.ts
+++ index.ts
@@ -2,7 +2,6 @@
  * One of the most common uses of interfaces in languages like C# and Java, 
  * that of explicitly enforcing that a class meets a particular contract, is also possible in TypeScript.
  */
-
 interface ClockInterface {
   currentTime: Date;
 }
@@ -16,7 +15,6 @@ class Clock implements ClockInterface {
  * You can also describe methods in an interface that are implemented in the class, 
  * as we do with setTime in the below example:
  */
-
 interface ClockInterfaceOne {
     currentTime: Date;
     setTime(d: Date);
@@ -32,13 +30,15 @@ class ClockOne implements ClockInterfaceOne {
 
 /**
  * Interfaces describe the public side of the class, rather than both the public and private side. 
- * This prohibits you from using them to check that a class also has particular types for the private side of the class instance.
+ * This prohibits you from using them to check that a class also has particular 
+ * types for the private side of the class instance.
  */
 
 /**
- * Difference between the static and instance sides of classes
+ * Difference between the static and instance sides of classes:
  * When working with classes and interfaces, it helps to keep in mind that a class has two types: 
- * the type of the static side (no need to instantiate the class. i.e. Array.from(), Array.isArray()) and the type of the instance side. 
+ * the type of the static side (no need to instantiate the class. i.e. Array.from(), Array.isArray()) 
+ * and the type of the instance side (var array = new Array();). 
  * You may notice that if you create an interface with a construct signature and try to create a class 
  * that implements this interface you get an error:
  */
@@ -48,15 +48,124 @@ class ClockOne implements ClockInterfaceOne {
 
  // error: "Class 'ClockTwo' incorrectly implements interface 'ClockConstructor'. 
  // type 'ClockTwo' provides no match for the signature 'new (hour: number, minute: number): any'"
+
  class ClockTwo implements ClockConstructor {
    currentTime: Date;
-   constructor(h: number, m: number) { } // this is not an instance, but a static side of the class
+   constructor(h: number, m: number) { } // this is not an instance, but the static side of the class
  }
-
 /**
  * This is because when a class implements an interface, only the instance side of the class is checked. 
  * Since the constructor sits in the static side, it is not included in this check.
+ * Instead, you would need to work with the static side of the class directly.
+ *  
+ * In this example, we define two interfaces, ClockCtructor for the constructor and 
+ * ClockInterface for the instance methods. Then for convenience we define 
+ * a constructor function createClock that creates instances of the type that is passed to it.
  */
+
+// constructs a class whose constructor takes hour and minute and returns an obj of type 'ClockIface'
+interface ClockCtructor {
+  new (hour: number, minute: number, timeOfTheDay: string): ClockIface;
+}
+
+// the interface establishes what properties/methods a class needs to have
+interface ClockIface {
+  hour: number; // property be created in the constructor
+  minute: number; // property be created in the constructor
+  timeOfTheDay: string; // property be created in the constructor
+  currentTime: any; //property to be returned from 'getTime'
+  tick(); // dummy method
+  setTime(); // sets time
+  getTime(); // gets time
+}
+
+// this is just a convenience method used to instantiate the classes
+// we can instantiate the classes directly if we want (i.e. new DigitalClock(7, 34, 'p.m.'))
+// it takes the 'ClockCtructor', the hour, and the minute, and returns an obj of type 'ClockIface'
+function createClock(ctor: ClockCtructor, hour: number, minute: number, timeOfTheDay: string): ClockIface {
+  // needs to enforce that 'DigitalClock' and 'AnalogClock' meet the contract established on 'ClockCtructor'
+  return new ctor(hour, minute, timeOfTheDay);
+}
+
+// parent class from which we will extend other 'clock' classes
+class ClockParent implements ClockIface {
+  hour: number; 
+  minute: number; 
+  timeOfTheDay: string;
+  currentTime: any; 
+
+  constructor(h: number, m: number, tOfTheDay: string) { 
+    this.hour = h;
+    this.minute = m;
+    this.timeOfTheDay = tOfTheDay;
+  }
+
+  tick() {}
+  setTime(){
+    this.currentTime = this.hour + ':' + this.minute + ' ' + this.timeOfTheDay;
+  }
+  getTime(){
+    return this.currentTime;
+  } 
+}
+
+// instanciates parent class implicitly
+class DigitalClock extends ClockParent {
+  // overrides parent method
+  tick() {
+    return "beep beep: it is";
+  }
+}
+
+// instanciates parent class implicitly
+class AnalogClock extends ClockParent {
+  // overrides parent method
+  tick() {
+    return "tick tock: it is";
+  }
+}
+
+// creates instances of clock classes
+let digital = createClock(DigitalClock, 12, 17, 'p.m.'); // instantiates with convenience method
+let digitalTwo = new DigitalClock(7, 34, 'p.m.'); // this is the same as the above
+let analog = createClock(AnalogClock, 7, 32,  'a.m.');
+
+let clockLogger = (clockInstance) => {
+  clockInstance.setTime();
+  // clockInstance.constructor['name'] gets class name; type-checker yells if we use 'constructor.name'
+  // unless we assign type 'any' to the constructor and get the name from the variable:
+  // let class: any = digital.constructor; console.log(class.name);
+  console.log(clockInstance.constructor['name'] + ' ' + clockInstance.tick() + ' ' + clockInstance.getTime());
+};
+
+clockLogger(digital);
+clockLogger(digitalTwo);
+clockLogger(analog);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
