288906964cc735d9f6739a5b64506e1e39bdac09
Gabriel Ferraz <gferraz@fullsail.com>
2017-03-24 16:38:29 -0400

    adds more code

--- index.js
+++ index.js
@@ -11,10 +11,18 @@ var Boat = (function () {
     function Boat(engine, color) {
         this.engine = engine;
         this.color = color;
-        this.engine = engine;
-        this.color = color;
     }
     return Boat;
 }());
 var yellowBoat = new Boat('BMW', 'yellow');
 console.log(yellowBoat);
+var Plane = (function () {
+    // the 'public' keyword creates the properties on this class automatically
+    function Plane(engine, color) {
+        this.engine = engine;
+        this.color = color;
+    }
+    return Plane;
+}());
+var yellowPlane = new Plane('Embraer', 'yellow');
+console.log(yellowPlane);
--- index.ts
+++ index.ts
@@ -17,14 +17,19 @@ let yellowFord = new Car({engine: 'Ford', color: 'yellow'});
 console.log(yellowFord);
 
 // OR this way (cleaner)
-
+////////////////////////
 interface IBoat{  
   engine: string;
   color: string;
 }
 
 class Boat implements IBoat{  
-  constructor (public engine: string, public color: string) {
+  // need to declare these two props of the class
+  // they are public by default but we can add the keyword if we want
+  public engine: string; 
+  public color: string;
+
+  constructor (engine: string, color: string) {
     this.engine = engine;
     this.color = color;
   }
@@ -32,3 +37,19 @@ class Boat implements IBoat{
 
 let yellowBoat = new Boat('BMW', 'yellow');
 console.log(yellowBoat);
+
+//OR even this way (even cleaner)
+//////////////////
+interface IPlane{  
+  engine: string;
+  color: string;
+}
+
+class Plane implements IPlane{  
+  // the 'public' keyword creates the properties on this class automatically
+  constructor (public engine: string, public color: string) {}
+} 
+
+let yellowPlane = new Plane('Embraer', 'yellow');
+console.log(yellowPlane);
+
